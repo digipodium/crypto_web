d24966f02bc2490954c2780dd7263282bcdf54f3
zaid kamil <xaidmetamorphos@gmail.com>
2021-06-29 18:39:40 +0530

    models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,5 @@
+from app.models import Currency
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Currency)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.db.models.fields import CharField, DateTimeField, TextField, URLField
 
 # Create your models here.
+
+class Currency(models.Model):
+    name = CharField(max_length=25)
+    website = URLField(max_length=255)
+    added_on = DateTimeField(auto_now=True)
+    class Meta:
+        verbose_name = 'Currency'
+        verbose_name_plural = 'Currencies'
+
+    def __str__(self):
+        return self.name
+
+class Article(models.Model):
+    """news article for crypto currencies copied from other places."""
+
+    title = CharField(max_length=50)
+    detail = TextField()
+    date = DateTimeField(auto_now=True)
+    author = CharField(max_length=25)
+    category = CharField(max_length=10)
+    image = Image
+
+    class Meta:
+        """Meta definition for Article."""
+
+        verbose_name = 'Article'
+        verbose_name_plural = 'Articles'
+
+    def __str__(self):
+        """Unicode representation of Article."""
+        pass
+
+
